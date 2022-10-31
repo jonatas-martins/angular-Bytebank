12710036309b7121db12d09240bf444301e5a868
Jonatas Martins <107153976+jonatas-martins@users.noreply.github.com>
2022-10-31 17:02:54 -0300

    feat: starter functions

--- bytebank/src/app/app.module.ts
+++ bytebank/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NovaTransfernciaComponent } from './nova-transfencia/nova-transferencia.componente';
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ExtratoComponent } from './extrato/extrato.component';
+import localept from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localept, 'pt');
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NovaTransfernciaComponent
+  declarations: [AppComponent, NovaTransfernciaComponent, ExtratoComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL',
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- bytebank/src/app/nova-transfencia/nova-transferencia.componente.ts
+++ bytebank/src/app/nova-transfencia/nova-transferencia.componente.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.componente.html',
   styleUrls: ['./nova-transferencia.componente.scss'],
 })
-export class NovaTransfernciaComponent {}
+export class NovaTransfernciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log('Transferencia Solicitada');
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
+    this.limparcampos();
+  }
+
+  limparcampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
